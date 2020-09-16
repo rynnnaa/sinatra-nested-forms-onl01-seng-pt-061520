89c7b2fdad04409374c67c09183c306bca5aba42
Ryna Truong <rynatruong@outlook.com>
2020-09-16 20:14:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,9 +14,14 @@ module FormsLab
     end
     
     post '/pirates' do
-      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
-      @ship1 = Ship.new(params[:pirate][:ships][0][:name],params[:pirate][:ships][0][:type], params[:pirate][:ships][0][:booty])
-      @ship2 = Ship.new(params[:pirate][:ships][1][:name],params[:pirate][:ships][1][:type], params[:pirate][:ships][1][:booty])
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+      erb :"/pirates/show"
+    end
       
       erb :'pirates/show'
     end
