242edc928b41fc4fd2c33b1b8e10356a7aa44470
Ryna Truong <rynatruong@outlook.com>
2020-09-16 19:40:43 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  
+  attr_reader :name, :weight, :height
+  @@all = []
+  
+  def initialize(params)
+    @name = name
+    @weight = weight
+    @height = height
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  
+  attr_reader :name, :type, :booty
+  @@all = []
+  
+  def initialize(params)
+    @name = name
+    @type = type
+    @booty = booty
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+  def self.clear
+    @@all = []
+  end
+  
+  
 end
